11607cbdeacabdaf77063dd882eaedf6fbbbbaee
Filips Barakats <philipe.barakat@yahoo.com>
2022-05-18 21:48:09 +0300

    publish

--- src/main/java/spring/website/supermaket/Application.java
+++ src/main/java/spring/website/supermaket/Application.java
@@ -1,4 +1,4 @@
-package com.example.application;
+package spring.website.supermaket;
 
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
